1df8fd614ce6d2221bd4b0ac35a2cfda13459058
liuchao <liuchao@med-log.cn>
2016-02-25 17:41:25 +0800

    版本控制

--- WebContent/Content/script/app/service/ApiPath.js
+++ WebContent/Content/script/app/service/ApiPath.js
@@ -3,8 +3,8 @@ console.log("接口控制启动");
 var ApiPath = {
     /// <summary>Api访问接口服务</summary>
     //Path: "http://192.168.0.130:8080/oms-api",
-    //Path: "http://115.28.129.13:8081/oms-api",
-    Path: "http://192.168.1.84:8081/oms-api",
+    Path: "http://115.28.129.13:8081/oms-api",
+    //Path: "http://192.168.1.84:8081/oms-api",
     //Path: "http://omsapiv2.med-log.cn:8081/oms-api",
     Account: {
         /// <summary>用户地址</summary>
