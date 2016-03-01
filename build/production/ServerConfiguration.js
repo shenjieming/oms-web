86764651ec103f6265f56c19c7280747739ebef4
xuezhiyu <xuezhiyu@med-log.cn>
2016-03-01 15:02:28 +0800

    修改配置文件以及脚本

---
+++ build/production/ServerConfiguration.js
@@ -0,0 +1,9 @@
+﻿
+
+var ServerConfiguration = {
+    /// <summary>Api访问接口服务</summary>
+    //Path: "http://192.168.0.130:8080/oms-api",
+    //Path: "http://115.28.129.13:8081/oms-api",
+    //Path: "http://192.168.1.84:8081/oms-api",
+    Path: "http://scm.med-log.cn/oms-api",
+}
