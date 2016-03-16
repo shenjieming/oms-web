a1afaa070154765b435bfc84d3b70a3902a1b0ad
liuchao <liuchao@med-log.cn>
2016-03-16 15:59:28 +0800

    地址插件优化

--- WebContent/Content/script/app/Directive/areaDirective.js
+++ WebContent/Content/script/app/Directive/areaDirective.js
@@ -10,18 +10,22 @@ app.directive("ngArea", function ($Api, $MessagService) {
     /// <summary>地区级联管理标签</summary>
     return {
         restrict: "EA",
-        templateUrl: "Content/script/app/Directive/ui/ngArea.html",
+        templateUrl: "Content/script/app/Directive/ui/ngArea.html?data=" + Timestamp,
         scope: {
             ngModel: '=',
-            ngData:"="
+            ngData: "="
         },
         replace: true,
         link: function ($scope, element, attrs) {
+            var Original = {
+                IsNewModel: false
+            }
+
             $scope.AreaController = {
                 /// <summary>地区管理控制器</summary>
                 Province: new Array(),
                 City: new Array(),
-                Township:new Array(),
+                Township: new Array(),
                 GetProvince: function () {
                     /// <summary>获取省信息</summary>
                     $MessagService.loading("地区获取中，请稍等...");
@@ -31,7 +35,7 @@ app.directive("ngArea", function ($Api, $MessagService) {
                 },
                 GetCity: function (code) {
                     /// <summary>获取市信息</summary>
-                    $scope.ngModel.deliveryCityCode = "";
+                    $scope.ngModel.deliveryCityCode = Original.IsNewModel ? $scope.ngModel.deliveryCityCode : "";
                     if (code) {
                         $MessagService.loading("地区获取中，请稍等...");
                         $Api.BasisService.GetadmdivisionList({ level: 2, parentDivCode: code }, function (rData) {
@@ -41,27 +45,49 @@ app.directive("ngArea", function ($Api, $MessagService) {
                 },
                 GetTownship: function (code) {
                     /// <summary>获取区信息</summary>
-                    $scope.ngModel.deliveryDistrictCode = "";
+                    $scope.ngModel.deliveryDistrictCode = Original.IsNewModel ? $scope.ngModel.deliveryDistrictCode : "";
                     if (code) {
                         $MessagService.loading("地区获取中，请稍等...");
                         $Api.BasisService.GetadmdivisionList({ level: 3, parentDivCode: code }, function (rData) {
                             $scope.AreaController.Township = rData.rows;
+                            if (Original.IsNewModel) {
+                                setTimeout(function () {
+                                    Original.IsNewModel = false;
+                                },500);
+                              
+                            }
                         });
-                    } else { $scope.AreaController.Township = new Array();}
+                    } else { $scope.AreaController.Township = new Array(); }
                 }
             }
-           
+
+            $scope.$watch("ngModel", function () {
+                /// <summary>监控整体对象的变化</summary>
+                //当满足所有的数据都是被第三方修改时
+                if ($scope.ngModel.deliveryProvinceCode && $scope.ngModel.deliveryCityCode && $scope.ngModel.deliveryDistrictCode) {
+                    Original.IsNewModel = true;
+                    $scope.AreaController.GetProvince();
+                    $scope.AreaController.GetCity($scope.ngModel.deliveryProvinceCode);
+                    $scope.AreaController.GetTownship($scope.ngModel.deliveryCityCode);
+               
+                }
+            });
+
             $scope.$watch("ngModel.deliveryProvinceCode", function () {
                 /// <summary>省修改事件</summary>
-                if (!$scope.ngModel.deliveryProvinceCode && $scope.ngModel.deliveryProvinceCode != "") {
-                    $scope.AreaController.GetProvince();
+                if (!Original.IsNewModel) {
+                    if (!$scope.ngModel.deliveryProvinceCode && $scope.ngModel.deliveryProvinceCode != "") {
+                        $scope.AreaController.GetProvince();
+                    }
+                    $scope.AreaController.GetCity($scope.ngModel.deliveryProvinceCode);
                 }
-                $scope.AreaController.GetCity($scope.ngModel.deliveryProvinceCode);
             });
 
             $scope.$watch("ngModel.deliveryCityCode", function () {
                 /// <summary>市修改事件</summary>
-                $scope.AreaController.GetTownship($scope.ngModel.deliveryCityCode);
+                if (!Original.IsNewModel) {
+                    $scope.AreaController.GetTownship($scope.ngModel.deliveryCityCode);
+                }
             });
         }
     };
