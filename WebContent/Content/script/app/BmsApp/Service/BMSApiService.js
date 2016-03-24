b688bab3cbc30163a52940fa9863ee8d87d7d015
liuchao <liuchao@med-log.cn>
2016-03-24 16:22:31 +0800

    BMS基础框架搭建

---
+++ WebContent/Content/script/app/BmsApp/Service/BMSApiService.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="../../../lib/Jquery/jquery-1.4.4.min.js" />
+/// <reference path="../../../lib/angular-1.2.20/angular.min.js" />
+/// <reference path="../ApiPath.js" />
+/// <reference path="../../Config.js" />
+
+var BMSApiService = angular.module('BMSApiService', []);
+BMSApiService.service()
--- build/dev/ServerConfiguration.js
+++ build/dev/ServerConfiguration.js
@@ -1,9 +1,17 @@
-﻿
-
+﻿/*
+配置文件，为了将配置性的信息与常变的开发信息分离
+有效的减少多人开发在配置时产生的代码覆盖
+*/
 var ServerConfiguration = {
-    /// <summary>Api访问接口服务</summary>
-    //Path: "http://192.168.0.130:8080/oms-api",
-    //Path: "http://115.28.129.13:8081/oms-api",
-    //Path: "http://192.168.1.84:8081/oms-api",
-    Path: "http://192.168.1.84:8081/oms-api",
+    /// <summary>OMS服务配资</summary>
+    //OMS请求地址
+    //开启OMS系统
+    StartOMS: true,
+    OMSPath: "http://192.168.1.84:8081/oms-api",    //Path: "http://192.168.1.102:8080/oms-api",    //Path: "http://192.168.0.130:8080/oms-api",
+    //开启BMS系统
+    StartBMS: true,
+    //BMS请求地址
+    BMSPath: "http://192.168.1.84:8081/bms-api",
+    //是否启动开发,True的话全部菜单可用，false的话全部菜单与数据库的权限对接
+    IsDevelop: false
 }
