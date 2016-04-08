908a292bfc29b999aacf114c71f1e94c50040cf0
liuchao <liuchao@med-log.cn>
2016-04-08 17:05:19 +0800

    版本功能

--- WebContent/Content/script/app/ServerConfiguration.js
+++ WebContent/Content/script/app/ServerConfiguration.js
@@ -14,5 +14,6 @@ var ServerConfiguration = {
     //BMS请求地址
     BMSPath: "http://192.168.1.84:8081/bms-api",
     //是否启动开发,True的话全部菜单可用，false的话全部菜单与数据库的权限对接
+    Version: "dev",
     IsDevelop: true,
 }
