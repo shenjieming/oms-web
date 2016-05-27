6375ba23c098f38a51a8652219cf519d517051d2
shenjiem <shenjieming@med-log.cn>
2016-05-27 14:35:41 +0800

    计费管理预上线

--- build/test/ServerConfiguration.js
+++ build/test/ServerConfiguration.js
@@ -9,7 +9,7 @@ var ServerConfiguration = {
     StartOMS: true,
     OMSPath: "http://115.28.129.13:8081/oms-api",    //Path: "http://192.168.1.102:8080/oms-api",    //Path: "http://192.168.0.130:8080/oms-api",
     //开启BMS系统
-    StartBMS: false,
+    StartBMS: true,
     //BMS请求地址
     BMSPath: "http://115.28.129.13:8081/bms-api",
     //是否启动开发,True的话全部菜单可用，false的话全部菜单与数据库的权限对接
