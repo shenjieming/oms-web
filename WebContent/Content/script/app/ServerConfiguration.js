fddf7f20e33c91c0b2234aea9e1bfd46a69fb757
shenjiem <shenjieming@med-log.cn>
2016-06-03 10:50:58 +0800

    BMS开发环境开启

--- WebContent/Content/script/app/ServerConfiguration.js
+++ WebContent/Content/script/app/ServerConfiguration.js
@@ -14,9 +14,9 @@ var ServerConfiguration = {
     //开启BMS系统
     StartBMS: true,
     //BMS请求地址
-    BMSPath: "http://115.28.129.13:8081/oms-api",
+    BMSPath: "http://115.28.129.13:8081/bms-api",
     //是否启动开发,True的话全部菜单可用，false的话全部菜单与数据库的权限对接
     Version: "dev",
-    IsDevelop: false,
+    IsDevelop: true,
     PrintPath:"http://wmstest.med-log.cn/Reports/Pages/Report.aspx?ItemPath=%2freport+project1%2fpicklist"
 }
