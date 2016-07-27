ac45eea2c087b93b20bea9734aa0c015b5f5a6a3
shenjieming <495100766@qq.com>
2016-07-27 14:23:30 +0800

    开启计费系统

--- build/dev/ServerConfiguration.js
+++ build/dev/ServerConfiguration.js
@@ -14,7 +14,7 @@ var ServerConfiguration = {
     BMSPath: "http://192.168.1.84:8081/bms-api",
     //是否启动开发,True的话全部菜单可用，false的话全部菜单与数据库的权限对接
     Version: "${version}",
-    IsDevelop: true,
+    IsDevelop: false,
     EnvironmentDisplay:"（开发环境）",
     PrintPath:"http://wmstest.med-log.cn/Reports/Pages/Report.aspx?ItemPath=%2freport+project1%2fpicklist"
 }
--- build/production/ServerConfiguration.js
+++ build/production/ServerConfiguration.js
@@ -9,7 +9,7 @@ var ServerConfiguration = {
     StartOMS: true,
     OMSPath: "http://scm.med-log.com.cn/oms-api",
     //开启BMS系统
-    StartBMS: false,
+    StartBMS: true,
     //BMS请求地址
     BMSPath: "http://scm.med-log.com.cn/bms-api",
     //是否启动开发,True的话全部菜单可用，false的话全部菜单与数据库的权限对接
